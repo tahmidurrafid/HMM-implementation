f6dce2e5a6977d101b045bc2b53bf676f7fa5704
Md. Tahmidur Rafid <amtrafid@gmail.com>
2022-01-04 20:44:20 +0600

    File read

--- 1605046.py
+++ 1605046.py
@@ -1,1 +1,27 @@
-print("Hello world")
+import numpy
+import math
+
+class GDistribution:
+    def __init__(self, mean, stDeviation):
+        self.mean = mean
+        self.stDeviation = stDeviation
+    def print(self):
+        print("MEAN: " + str(self.mean) + ", stDeviation: " + str(self.stDeviation))
+
+data = numpy.loadtxt("Input/data.txt")
+paramFile = open("Input/parameters.txt.txt", "r")
+stateCount = int(paramFile.readline())
+transitionMatrix = []
+distributions = []
+
+for i in range(0, stateCount):
+    line = paramFile.readline()
+    arr = list(map(float, line.split()))
+    transitionMatrix.append(arr)
+transitionMatrix = numpy.array(transitionMatrix)
+
+matrix = [list(map(float, line.split())) for line in paramFile.read().split("\n")]
+paramFile.close()
+for i in range(0, stateCount):
+    distributions.append(GDistribution(matrix[0][i], math.sqrt(matrix[1][i]) ))
+
