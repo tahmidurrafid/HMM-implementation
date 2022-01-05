a795b044b96b128a7c1a8ad6670c2dbd9e1183f9
Md. Tahmidur Rafid <amtrafid@gmail.com>
2022-01-05 19:04:53 +0600

    Viterbi

--- 1605046.py
+++ 1605046.py
@@ -11,9 +11,23 @@ class GDistribution:
         self.stDeviation = stDeviation
     def print(self):
         print("MEAN: " + str(self.mean) + ", stDeviation: " + str(self.stDeviation))
+    def pdf(self, x):
+        return (1/(self.stDeviation*math.sqrt(2*math.pi)) )*math.exp(-((x-self.mean)**2)/(2*self.stDeviation*self.stDeviation))
+
+def calcStationaryDistribution(tranMatrix):
+    A = deepcopy(tranMatrix)
+    A = numpy.transpose(A)
+    A[0] = numpy.array(ones(len(A)))
+    for i in range(1, len(A)):
+        A[i][i] -= 1
+    B = numpy.zeros(len(A))
+    B[0] = 1
+    X = numpy.linalg.inv(A).dot(B)
+    return X
 
 data = numpy.loadtxt("Input/data.txt")
 paramFile = open("Input/parameters.txt.txt", "r")
+
 stateCount = int(paramFile.readline())
 transitionMatrix = []
 distributions = []
@@ -27,19 +41,42 @@ transitionMatrix = numpy.array(transitionMatrix)
 matrix = [list(map(float, line.split())) for line in paramFile.read().split("\n")]
 paramFile.close()
 for i in range(0, stateCount):
+    # distributions.append(GDistribution(matrix[0][i], matrix[1][i] ))
     distributions.append(GDistribution(matrix[0][i], math.sqrt(matrix[1][i]) ))
 
-def calcStationaryDistribution(tranMatrix):
-    A = deepcopy(tranMatrix)
-    A = numpy.transpose(A)
-    A[0] = numpy.array(ones(len(A)))
-    for i in range(1, len(A)):
-        A[i][i] -= 1
-    # print(A)
-    B = numpy.zeros(len(A))
-    B[0] = 1
-    # print(B)
-    X = numpy.linalg.inv(A).dot(B)
-    print(X)
+stDist = calcStationaryDistribution(transitionMatrix)
 
-calcStationaryDistribution(transitionMatrix)
+dp = numpy.full((len(data)+1, 2), -math.inf)
+parent = numpy.full((len(data), 2), -math.inf)
+path = numpy.zeros(len(data), dtype=int)
+dp[0] = calcStationaryDistribution(transitionMatrix)
+
+for i in range(0, stateCount):
+    dp[0][i] = math.log(dp[0][i]) + math.log(distributions[i].pdf(data[i]))
+print(dp[0])
+
+for j in range(1, len(data)):
+    for i in range(0, stateCount):
+        for k in range(0, stateCount):
+            val = dp[j-1][k] + math.log(transitionMatrix[k][i]*distributions[i].pdf(data[j]))
+            print("(" + str(-math.log(distributions[i].pdf(data[j]))) + ")", end=' ')
+            if(val >= dp[j][i]):
+                dp[j][i] = val
+                parent[j][i] = k
+        print()
+lastValue = -math.inf
+for i in range(0, stateCount):
+    if(dp[len(data)-1][i] > lastValue):
+        lastValue = dp[len(data)-1][i]
+        path[len(data)-1] = int(i)
+
+for i in range(len(data)-1, 0, -1):
+    path[i-1] = parent[i][path[i]]
+
+a_file = open("test.txt", "w")
+for row in path:
+    if(row == 0):
+        a_file.write("\"El Nino\"" + "\n" )        
+    else:
+        a_file.write("\"La Nina\"" + "\n" )
+a_file.close()
